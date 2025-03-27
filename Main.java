0cb8a1ac51a6ceef9a54f93faedf0b990a55f88d
Teo <144486916+TeooL@users.noreply.github.com>
2025-03-27 14:42:32 -0400

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,13 +1,10 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-
 public class Main{
     public static void main(String[] args){
         String[] departments = {"Biology", "Chemistry", "CTE", "English", "PE", "Mathematics", "Physics", "Social", "Arts", "Language"};
 
         int i = 1;
         for (String department: departments){
-            System.out.println("INSERT INTO Departments (d_id, Department) VALUES (" + i + ", " + departments[i-1] + ")");
+            System.out.println("INSERT INTO Departments (d_id, Department) VALUES (" + i + ",'" + departments[i-1] + "\');");
             i++;
         }
     }
